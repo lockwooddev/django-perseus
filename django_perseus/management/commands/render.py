9b44fd662122da2121b3fe7d4cf9d734722ca9fb
Carlo Smouter <carlo.smouter@moccu.com>
2015-01-10 16:51:20 +0100

    Fix --archive option on render management command. Optionally --filename option.

--- django_perseus/management/commands/render.py
+++ django_perseus/management/commands/render.py
@@ -7,21 +7,24 @@ from optparse import make_option
 
 class Command(BaseCommand):
 
-    option_list = BaseCommand.option_list + (make_option(
-        '--archive',
-        action='store_true',
-        dest='archive',
-        default=False,
-        help='Zips the result of the statically generated website'),
-        make_option('--filename',
-        action='store',
-        dest='filename',
-        default=''),
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--archive',
+            action='store_true',
+            dest='archive',
+            default=False,
+            help='Zips the result of the statically generated website'),
+        make_option(
+            '--filename',
+            action='store',
+            dest='filename',
+            default=''),
     )
 
     def handle(self, *args, **options):
         run_renderers()
         run_importers()
 
-        if options['archive']:
+        archive = options.get('archive')
+        if archive:
             zip_dir(options.get('filename', 'render.zip'))
